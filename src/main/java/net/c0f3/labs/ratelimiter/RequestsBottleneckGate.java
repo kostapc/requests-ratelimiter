013129d86aec74846bef6a31fd15c8313613cb44
kostapc <kostapc@gmail.com>
2024-09-17 01:21:59 +0300

    RequestsBottleneckGate concurrency test

--- src/main/java/net/c0f3/labs/ratelimiter/RequestsBottleneckGate.java
+++ src/main/java/net/c0f3/labs/ratelimiter/RequestsBottleneckGate.java
@@ -5,10 +5,16 @@ import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Refill;
 
 import java.time.Duration;
-import java.util.Optional;
 import java.util.function.Supplier;
 
+/**
+ * <a href="https://bucket4j.com/8.14.0/toc.html">Bucket4j docs</a>
+ *
+ * @param <R> - call result type
+ */
 public class RequestsBottleneckGate<R> {
+
+    private static final int TOTAL_TIMEOUT_MILLS = 1000 * 60 - 500;
     private final Bucket bucket;
 
     public RequestsBottleneckGate() {
@@ -19,14 +25,32 @@ public class RequestsBottleneckGate<R> {
             .build();
     }
 
-    public Optional<R> doRequest(Supplier<R> request) {
+    public R doRequest(Supplier<R> request) {
+        var bb = bucket.asBlocking();
         try {
-            if (bucket.asBlocking().tryConsume(1, Duration.ofSeconds(10))) {
-                return Optional.ofNullable(request.get());
-            }
+            bb.tryConsume(1, Duration.ofMillis(TOTAL_TIMEOUT_MILLS));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        return Optional.empty();
+        return request.get();
+
+        /*var start = System.currentTimeMillis();
+        var passedTime = 0L;
+        var counter = 0;
+        while (passedTime < TOTAL_TIMEOUT_MILLS) {
+            counter++;
+            try {
+                if (bb.tryConsume(1, Duration.ofSeconds(1))) {
+                    return request.get();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException("interrupted on retry " + counter, e);
+            } catch (Exception e) {
+                throw new RuntimeException("failed on retry " + counter, e);
+            }
+            passedTime = System.currentTimeMillis() - start;
+        }
+        throw new RuntimeException("call unavailable");*/
     }
+
 }
--- src/test/java/com/arnoldgalovics/blog/ratelimiterfeign/RateLimiterTest.java
+++ src/test/java/com/arnoldgalovics/blog/ratelimiterfeign/RateLimiterTest.java
@@ -2,8 +2,11 @@ package com.arnoldgalovics.blog.ratelimiterfeign;
 
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.google.common.util.concurrent.AtomicDouble;
 import net.c0f3.labs.ratelimiter.RequestsBottleneckGate;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +14,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
@@ -31,22 +39,25 @@ class RateLimiterTest {
     @Autowired
     private UserSessionClient userSessionClient;
 
-    @Test
-    public void testRateLimiterWorks() throws Exception {
-        String responseBody = "{ \"sessionId\": \"828bc3cb-52f0-482b-8247-d3db5c87c941\", \"valid\": true}";
+    private static final UUID uuid = UUID.fromString("828bc3cb-52f0-482b-8247-d3db5c87c941");
 
-        String uuidString = "828bc3cb-52f0-482b-8247-d3db5c87c941";
-        UUID uuid = UUID.fromString(uuidString);
+    @BeforeEach
+    public void prepareMock() {
+        String responseBody = "{ \"sessionId\": \"" + uuid + "\", \"valid\": true}";
 
         USER_SESSION_SERVICE.stubFor(
             get(urlPathEqualTo("/user-sessions/validate"))
-                .withQueryParam("sessionId", equalTo(uuidString))
+                .withQueryParam("sessionId", equalTo(uuid.toString()))
                 .willReturn(aResponse()
                     .withBody(responseBody)
                     .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
                     .withFixedDelay(10)
                 )
         );
+    }
+
+    @Test
+    public void testRateLimiterWorks() {
 
         final int count = 6;
 
@@ -58,49 +69,90 @@ class RateLimiterTest {
             futures.add(f);
         }
         var counter = new AtomicInteger(1);
-        var time = System.currentTimeMillis();
-        Assertions.assertThrows(RuntimeException.class, () -> {
-                futures.forEach(f -> {
-                    try {
-                        f.get();
-                        System.out.println("resp " + counter.getAndIncrement());
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-        });
-        time = System.currentTimeMillis() - time;
-        System.out.println("passed " + time + " mills");
+        Assertions.assertThrows(RuntimeException.class, () -> futures.forEach(f -> {
+            try {
+                f.get();
+                System.out.println("resp " + counter.getAndIncrement());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }));
     }
 
     @Test
-    public void testRateLimiterWithBucketWorks() throws Exception {
-        String responseBody = "{ \"sessionId\": \"828bc3cb-52f0-482b-8247-d3db5c87c941\", \"valid\": true}";
+    public void testRateLimiterWithBucketWorks() {
 
-        String uuidString = "828bc3cb-52f0-482b-8247-d3db5c87c941";
-        UUID uuid = UUID.fromString(uuidString);
+        var count = 10;
 
-        USER_SESSION_SERVICE.stubFor(get(urlPathEqualTo("/user-sessions/validate"))
-            .withQueryParam("sessionId", equalTo(uuidString))
-            .willReturn(aResponse().withBody(responseBody).withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE).withFixedDelay(500)));
+        ExecutorService executorService = Executors.newFixedThreadPool(count);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-
-        var limitatator = new RequestsBottleneckGate<UserSessionValidationResponse>();
+        var limitator = new RequestsBottleneckGate<UserSessionValidationResponse>();
 
         List<Future<?>> futures = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < count; i++) {
             Future<?> f = executorService.submit(
-                () -> limitatator.doRequest(() -> userSessionClient.validateSession(uuid))
+                () -> limitator.doRequest(
+                    () -> userSessionClient.validateSession(uuid)
+                )
             );
             futures.add(f);
         }
-        futures.forEach(f -> {
+        for (Future<?> f : futures) {
             try {
                 f.get();
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-        });
+        }
+    }
+
+    @Test
+    public void testHightPressureRateLimiterWithBucketWorks() {
+        var totalCount = 200;
+        var executorService = new ThreadPoolExecutor(
+            50, 50, 0L,
+            TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(totalCount),
+            Executors.defaultThreadFactory(),
+            // invocation by caller thread
+            (r, exe) -> {
+                org.assertj.core.api.Assertions.fail("rejected execution call");
+            }
+        );
+
+        var limitator = new RequestsBottleneckGate<UserSessionValidationResponse>();
+
+        var log = new ArrayList<>(totalCount);
+        var ave = new AtomicDouble(0);
+        List<Future<UserSessionValidationResponse>> futures = new ArrayList<>();
+        for (int i = 0; i < totalCount; i++) {
+            final int ii = i;
+            var f = executorService.submit(
+                () -> {
+                    var time = System.currentTimeMillis();
+                    var res = limitator.doRequest(
+                        () -> userSessionClient.validateSession(uuid)
+                    );
+                    time = System.currentTimeMillis() - time;
+                    log.add(ii + ") " + (time/1000) + " sec.\t" + time);
+                    ave.set((time + ave.get() * ii)/(ii+1));
+                    return res;
+                }
+            );
+            futures.add(f);
+        }
+        var failFlag = new AtomicBoolean(false);
+        for (Future<UserSessionValidationResponse> f : futures) {
+            try {
+                var res = f.get();
+                Assertions.assertNotNull(res);
+            } catch (Exception e) {
+                e.printStackTrace();
+                failFlag.set(true);
+                break;
+            }
+        }
+        log.forEach(System.out::println);
+        System.out.println("> total count: " + log.size() + "; avg time: " + ave.get());
+        Assertions.assertFalse(failFlag.get(), "one of executions failed");
     }
 }
